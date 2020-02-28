2cf9756c872bb6bd03989ebe1fab01b153150e11
Maxim Orlov <maximorl@gmail.com>
2020-02-28 17:50:58 +0700

    Stop deploy script on errors, quote variables

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
+# Stop script on first error
+set -e
+
 IMAGE_NAME="maximization/autodeploy-docker"
 IMAGE_TAG=$(git rev-parse --short HEAD) # first 7 characters of the current commit hash
 
 echo "Building Docker image ${IMAGE_NAME}:${IMAGE_TAG}, and tagging as latest"
-docker build -t ${IMAGE_NAME}:${IMAGE_TAG} .
-docker tag ${IMAGE_NAME}:${IMAGE_TAG} ${IMAGE_NAME}:latest
+docker build -t "${IMAGE_NAME}:${IMAGE_TAG}" .
+docker tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:latest"
 
 echo "Authenticating and pushing image to Docker Hub"
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-docker push ${IMAGE_NAME}:${IMAGE_TAG}
-docker push ${IMAGE_NAME}:latest
+docker push "${IMAGE_NAME}:${IMAGE_TAG}"
+docker push "${IMAGE_NAME}:latest"
