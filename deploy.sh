d8ed754b9d4d94e36e492c7f4d7cd80018c49e36
Maxim Orlov <maximorl@gmail.com>
2020-03-01 15:23:49 +0700

    Fix public key addition to known_hosts

--- deploy.sh
+++ deploy.sh
@@ -20,14 +20,14 @@ echo "${SSH_KEY}" | base64 -d > ssh_key
 chmod 600 ssh_key # private keys need to have strict permission to be accepted by SSH agent
 
 # Add production server to known hosts
-echo "${SERVER_PUBLIC_KEY}" | base64 -d >> "~/.ssh/known_hosts"
+echo "${SERVER_PUBLIC_KEY}" | base64 -d >> ~/.ssh/known_hosts
 
 echo "Deploying via remote SSH"
 ssh -i ssh_key "root@${SERVER_IP}" \
-  'docker pull '"${IMAGE_NAME}:${IMAGE_TAG}"' \
-  && docker stop autodeploy-docker \
-  && docker rm autodeploy-docker \
-  && docker run --init -d --name autodeploy-docker -p 80:3000 '"${IMAGE_NAME}:${IMAGE_TAG}"' \
-  && docker system prune -af' # remove unused images to free up space
+  'docker pull '"${IMAGE_NAME}:${IMAGE_TAG}" \
+  '&& docker stop autodeploy-docker' \
+  '&& docker rm autodeploy-docker' \
+  '&& docker run --init -d --name autodeploy-docker -p 80:3000 '"${IMAGE_NAME}:${IMAGE_TAG}" \
+  '&& docker system prune -af' # remove unused images to free up space
 
 echo "Successfully deployed, hooray!"
