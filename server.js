b0ffa531909c21bfc661b0fc243fd49ec78b8357
Maxim Orlov <maximorl@gmail.com>
2020-03-01 16:08:52 +0700

    Simplify remote ssh command, visually test auto-deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  return res.end('hello world');
+  return res.end('hello world v2');
 });
 
 module.exports = server;
