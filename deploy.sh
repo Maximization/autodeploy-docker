a5e9b288f3af555f3d5f3d6bb90957c73b52b30b
Maxim Orlov <maximorl@gmail.com>
2020-03-01 14:56:20 +0700

    Add deploy section to script

--- deploy.sh
+++ deploy.sh
@@ -14,3 +14,20 @@ echo "Authenticating and pushing image to Docker Hub"
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker push "${IMAGE_NAME}:${IMAGE_TAG}"
 docker push "${IMAGE_NAME}:latest"
+
+# Decode SSH key
+echo "${SSH_KEY}" | base64 -d > ssh_key
+chmod 600 ssh_key # private keys need to have strict permission to be accepted by SSH agent
+
+# Add production server to known hosts
+echo "${SERVER_PUBLIC_KEY}" | base64 -d >> "~/.ssh/known_hosts"
+
+echo "Deploying via remote SSH"
+ssh -i ssh_key "root@${SERVER_IP}" \
+  'docker pull '"${IMAGE_NAME}:${IMAGE_TAG}"' \
+  && docker stop autodeploy-docker \
+  && docker rm autodeploy-docker \
+  && docker run --init -d --name autodeploy-docker -p 80:3000 '"${IMAGE_NAME}:${IMAGE_TAG}"' \
+  && docker system prune -af' # remove unused images to free up space
+
+echo "Successfully deployed, hooray!"
